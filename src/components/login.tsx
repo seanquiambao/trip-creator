58199a530b1c4629847748c600254d2444823e21
seanquiambao <slquiambao03@gmail.com>
2025-02-06 14:03:33 -0800

    login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,45 +1,26 @@
 "use client";
 import { useState } from "react";
-import { auth, db } from "@/utils/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-
+import { api } from "@/utils/api";
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
   const [error, setError] = useState("");
-  const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!auth || !db) {
-      setError("Firebase is not initialized.");
-      return;
-    }
-
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredential.user;
-
-      const userDocRef = doc(db, "users", user.uid);
-      const userDoc = await getDoc(userDocRef);
-
-      let username = "User";
-      if (userDoc.exists()) {
-        username = userDoc.data().username || "User";
-      }
-
-      router.push(`/home?username=${encodeURIComponent(username)}`);
+      await api({
+        method: "POST",
+        url: "/api/login",
+        body: form,
+      });
     } catch (err) {
-      setError("Failed to log in. Check your credentials.");
+      setError(`${err}`);
       console.error("Login Error:", err);
     }
   };
@@ -63,21 +44,21 @@ const Login = () => {
             type="email"
             placeholder="Email"
             className="p-3 border border-gray-300 rounded-lg mb-4 w-full"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
             required
           />
           <input
             type="password"
             placeholder="Password"
             className="p-3 border border-gray-300 rounded-lg mb-4 w-full"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
             required
           />
           <button
             type="submit"
-            className="bg-[#2E627E] text-white font-bold p-3 rounded-lg w-full"
+            className="bg-trip-navy text-white font-bold p-3 rounded-lg w-full"
           >
             Login
           </button>
