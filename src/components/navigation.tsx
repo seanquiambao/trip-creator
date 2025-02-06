453334e7171e635410c016de4d35c8e0a1df93b4
seanquiambao <slquiambao03@gmail.com>
2025-02-06 14:29:36 -0800

    navigation done

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,26 +5,21 @@ import logo from "../app/logo.png";
 
 const Navigation: React.FC = () => {
   return (
-    <nav
-      style={{ backgroundColor: "#2E627E" }}
-      className="p-4 flex items-center justify-between shadow-md"
-    >
+    <nav className="bg-trip-navy p-4 flex items-center justify-between shadow-md">
       <div className="flex items-center">
         <Link href="/">
-          <button className="flex items-center">
-            <Image
-              src={logo}
-              alt="Logo"
-              width={50}
-              height={50}
-              className="mr-2"
-            />
-          </button>
+          <Image
+            src={logo}
+            alt="Logo"
+            width={50}
+            height={50}
+            className="mr-2"
+          />
         </Link>
       </div>
-      <a href="/" className="text-white hover:text-blue-300 font-poppins">
+      <Link href="/" className="text-white hover:text-blue-300 font-poppins">
         Logout
-      </a>
+      </Link>
     </nav>
   );
 };
