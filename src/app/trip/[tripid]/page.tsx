4bee6c9f5b16cb912ca24d0691dfc77176df06cb
seanquiambao <slquiambao03@gmail.com>
2025-02-11 18:47:58 -0800

    build error

--- src/app/trip/[tripid]/page.tsx
+++ src/app/trip/[tripid]/page.tsx
@@ -1,9 +1,9 @@
 import Activity from "@/components/activity";
-
 const Page = () => {
-  return <div>Trip Page</div>;
   return (
-    <div className="min-h-screen bg-trip-navy flex items-center justify-center p-4"></div>
+    <div className="min-h-screen bg-trip-navy flex items-center justify-center p-4">
+      <Activity />
+    </div>
   );
 };
 
