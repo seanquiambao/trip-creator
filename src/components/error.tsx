32471291b25572b86b9db60593a79c8dd673ac9c
Sean Quiambao <slquiambao03@gmail.com>
2025-02-13 20:18:13 -0800

    Update src/components/error.tsx

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -8,7 +8,7 @@ interface ErrorProps {
 const Error: React.FC<ErrorProps> = ({ code, message }) => {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-trip-navy text-white">
-      <div className="text-5xl font-bold text-[#589FA3]">{code}</div>
+      <div className="text-5xl font-bold text-white">{code}</div>
       <div className="text-2xl font-bold mb-4 text-black">{message}</div>
     </div>
   );
