18e2b857be70b7bb7b31da95a96149263a08a36f
seanquiambao <slquiambao03@gmail.com>
2025-02-13 18:40:35 -0800

    fix

--- src/components/activity.tsx
+++ src/components/activity.tsx
@@ -1,28 +1,27 @@
+"use client";
 import { X, Clock, MapPin, DollarSign } from "lucide-react";
 
 const Activity = () => {
   return (
-    <div className="group relative p-4 flex flex-col gap-2 text-white">
-      {/* Title */}
-      <h2 className="text-lg font-bold">Ono Hawaiian BBQ</h2>
+    <div className="group relative p-4 flex flex-col gap-2 text-white w-full">
+      <div className="flex flex-row justify-between">
+        <div className="text-lg font-bold">Ono Hawaiian BBQ</div>
+        <X
+          size={20}
+          className="cursor-pointer text-white hover:text-white/20"
+          onClick={() => console.log("Hello world")}
+        />
+      </div>
 
-      {/* Details */}
-      <div className="flex items-center gap-2 text-black/20">
+      <div className="flex items-center gap-2 text-white/20">
         <Clock size={16} /> <span>8 PM</span>
       </div>
-      <div className="flex items-center gap-2 text-gray-500">
+      <div className="flex items-center gap-2 text-white/20">
         <MapPin size={16} /> <span>1000 Main St.</span>
       </div>
-      <div className="flex items-center gap-2 text-gray-500">
+      <div className="flex items-center gap-2 text-white/20">
         <DollarSign size={16} /> <span>$100</span>
       </div>
-
-      {/* Hover Delete Icon */}
-      <X
-        size={20}
-        className="cursor-pointer text-white hover:text-white/20"
-        onClick={() => console.log("Hello world")}
-      />
     </div>
   );
 };
