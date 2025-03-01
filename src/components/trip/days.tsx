f6891a67f15844f7b764b815fac3072cd76de0bc
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2025-02-28 19:51:09 -0800

    wait how did this even get pushed lol

--- src/components/trip/days.tsx
+++ src/components/trip/days.tsx
@@ -44,71 +44,71 @@ const Days = ({ days, setDays }: Props) => {
       ].activities.filter((_, index) => index !== activityKey);
       setDays(updatedDays);
     }
-    const handleRemoveDay = (indexToRemove: number) => {
-      const filteredDays = days.filter((_, index) => index !== indexToRemove);
+  };
+  const handleRemoveDay = (indexToRemove: number) => {
+    const filteredDays = days.filter((_, index) => index !== indexToRemove);
 
-      const updatedDays = filteredDays.map((day, index) => ({
-        ...day,
-        day: index + 1,
-        date:
-          index < indexToRemove
-            ? day.date
-            : new Date(
-                filteredDays[index - 1].date.getTime() + 24 * 60 * 60 * 1000
-              ),
-      }));
+    const updatedDays = filteredDays.map((day, index) => ({
+      ...day,
+      day: index + 1,
+      date:
+        index < indexToRemove
+          ? day.date
+          : new Date(
+              filteredDays[index - 1].date.getTime() + 24 * 60 * 60 * 1000
+            ),
+    }));
 
-      setDays(updatedDays);
-    };
+    setDays(updatedDays);
+  };
 
-    return (
-      <div className="flex flex-col gap-4 w-full">
-        {days.map((day, index) => (
-          <div key={index} className="flex flex-row text-white w-full">
-            <div className="flex flex-col items-center justify-center">
-              <div className="flex items-center gap-6">
-                <X
-                  size={28}
-                  className="cursor-pointer text-white hover:text-white/50"
-                  onClick={() => handleRemoveDay(index)}
-                />
-                <div className="font-bold text-center">
-                  <div className="text-5xl">DAY</div>
-                  <div className="text-7xl">
-                    {day.day.toString().padStart(2, "0")}
-                  </div>
-                  <div className="text-white/20 text-2xl pt-4">
-                    {day.date.toLocaleDateString()}
-                  </div>
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      {days.map((day, index) => (
+        <div key={index} className="flex flex-row text-white w-full">
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex items-center gap-6">
+              <X
+                size={28}
+                className="cursor-pointer text-white hover:text-white/50"
+                onClick={() => handleRemoveDay(index)}
+              />
+              <div className="font-bold text-center">
+                <div className="text-5xl">DAY</div>
+                <div className="text-7xl">
+                  {day.day.toString().padStart(2, "0")}
+                </div>
+                <div className="text-white/20 text-2xl pt-4">
+                  {day.date.toLocaleDateString()}
                 </div>
               </div>
             </div>
-            <div className="gap-4 p-4 w-full grid">
-              <hr className="border-white/20 mb-2" />
-              {day.activities.length > 0 ? (
-                day.activities.map((activity, idx) => (
-                  <Activity
-                    key={idx}
-                    activityKey={idx}
-                    title={activity.title}
-                    time={activity.time}
-                    location={activity.location}
-                    cost={activity.cost}
-                    dayKey={index}
-                    handleDelete={handleDelete}
-                  />
-                ))
-              ) : (
-                <div className="font-bold text-white/20 text-2xl text-center">
-                  No Activities
-                </div>
-              )}
-            </div>
           </div>
-        ))}
-      </div>
-    );
-  };
+          <div className="gap-4 p-4 w-full grid">
+            <hr className="border-white/20 mb-2" />
+            {day.activities.length > 0 ? (
+              day.activities.map((activity, idx) => (
+                <Activity
+                  key={idx}
+                  activityKey={idx}
+                  title={activity.title}
+                  time={activity.time}
+                  location={activity.location}
+                  cost={activity.cost}
+                  dayKey={index}
+                  handleDelete={handleDelete}
+                />
+              ))
+            ) : (
+              <div className="font-bold text-white/20 text-2xl text-center">
+                No Activities
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Days;
