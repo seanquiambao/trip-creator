7f0cbd19b9336bb390efb8bae0fb5bb7afa49fde
nhaque11 <99585088+nhaque11@users.noreply.github.com>
2025-03-07 16:04:19 -0800

    test for day and activities

--- cypress/e2e/remove_place_day.cy.js
+++ cypress/e2e/remove_place_day.cy.js
@@ -5,16 +5,44 @@ describe("Remove Place and Remove Day", () => {
     });
   });
   it("should remove a place when clicking the X button", () => {
-    cy.get('[data-cy="remove-place-0"]') // Select the first place's remove button
-      .click();
+    cy.wait(1000);
 
-    cy.get('[data-cy="remove-place-0"]').should("not.exist");
+    // Get the initial length of activities
+    cy.get('[data-cy^="remove-place-0"]').then(($elements) => {
+      const initialLength = $elements.length;
+
+      // Click the first remove button
+      cy.get('[data-cy="remove-place-0"]')
+        .first() // Select the first element
+        .should("be.visible")
+        .click();
+
+      // Verify the length of activities is reduced by one
+      cy.get('[data-cy^="remove-place-0"]').should(
+        "have.length",
+        initialLength - 1
+      );
+    });
   });
 
-  it("should remove a day when clicking the X button", () => {
-    cy.get('[data-cy="remove-day-0"]') // Select the first day's remove button
-      .click();
+  it("should remove a place when clicking the X button", () => {
+    cy.wait(1000);
 
-    cy.get('[data-cy="remove-day-0"]').should("not.exist");
+    // Get the initial length of activities
+    cy.get('[data-cy^="remove-day-0"]').then(($elements) => {
+      const initialLength = $elements.length;
+
+      // Click the first remove button
+      cy.get('[data-cy="remove-day-0"]')
+        .first() // Select the first element
+        .should("be.visible")
+        .click();
+
+      // Verify the length of activities is reduced by one
+      cy.get('[data-cy^="remove-day-0"]').should(
+        "have.length",
+        initialLength - 1
+      );
+    });
   });
 });
